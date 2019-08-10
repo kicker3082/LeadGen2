d33b7c0f54d89804cc7930936ad96fb22a7b77ad
Keith Payne <keith.payne@creativesoftworks.com>
2019-08-10 16:08:06 -0400

    Added project to ingest items from Mass Idx. Added unit test project.

---
+++ MassIdxIngestion/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MassIdxIngestion
+{
+    /// <summary>
+    /// Data Object. Contains information about a MassIdx Listing that is pending
+    /// </summary>
+    public class PendingListing
+    {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ActiveListing
+    {
+
+    }
+}
