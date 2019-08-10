eeb977b7a60f0b1f4be800755e5b825fe69fc831
Keith Payne <keith.payne@creativesoftworks.com>
2019-08-10 15:30:42 -0400

    Initial creation of solution and 1 project.

---
+++ ServiceBusMessages/IngestedItemMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBusMessages
+{
+    /// <summary>
+    /// Data object containing the properties of a single ingested item.
+    /// </summary>
+    /// <remarks>This message occurrs when an item has been ingested into the system.</remarks>
+    public class IngestedItemMessage
+    {
+    }
+}
